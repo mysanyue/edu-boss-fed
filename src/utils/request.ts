e728f862260febfef5544c0556d158124e03a411
张伟 <myzony@qq.com>
2021-06-02 22:58:20 +0800

    commit

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,31 @@
 import axios from 'axios'
 import store from '@/store'
+import { Message } from 'element-ui'
+import router from '@/router'
+import qs from 'qs'
 
 const request = axios.create({
 
 })
 
+function redirectLogin() {
+  router.push({
+    name: 'login',
+    query: { redirect: router.currentRoute.fullPath }
+  })
+}
+
+function refreshToken() {
+  return axios.create()({
+    method: 'post',
+    url: '/front/user/refresh_token',
+    data: qs.stringify({ refreshtoken: store.state.user.refresh_token })
+  })
+}
+
+let isRfreshing = false // 控制刷新 token 的状态
+
+// 请求拦截器
 request.interceptors.request.use(config => {
   // 在这里通过改写 config 配置信息来实现业务功能的统一处理
   const { user } = store.state
@@ -13,6 +34,59 @@ request.interceptors.request.use(config => {
   }
   return config
 }, error => {
+  return Promise.reject(error)
+})
+
+// 响应拦截器
+request.interceptors.response.use(response => {
+  return response
+}, async error => {
+  if (error.response) {
+    // 请求发出去收到响应了，但是状态码超出了 2XX 范围
+    const { status } = error.response
+    if (status === 400) {
+      Message.error('请求参数错误')
+    } else if (status === 401) {
+      if (!store.state.user) {
+        redirectLogin()
+        return Promise.reject(error)
+      }
+
+      if (!isRfreshing) {
+        isRfreshing = true // 开启刷新状态
+        // 尝试刷新获取新的 token
+        return refreshToken().then(res => {
+          if (!res.data.success) {
+            throw new Error('刷新 Token 失败')
+          }
+
+          // 把刷新拿到的新的 access_token 更新到容器和本地存储中
+          store.commit('setUser', res.data.content)
+          // 把本次失败的请求重新发出去
+          return request(error.config)
+        }).catch(error => {
+          // 把当前用户登录状态清除
+          store.commit('setUser', null)
+          redirectLogin()
+          return Promise.reject(error)
+        }).finally(() => {
+          isRfreshing = false // 重置刷新状态
+        })
+      }
+    } else if (status === 403) {
+      Message.error('没有权限，请联系管理员')
+    } else if (status >= 500) {
+      Message.error('服务端错误，请联系管理员')
+    }
+  } else if (error.request) {
+    // 请求发出去没有收到响应
+    Message.error('请求超时，请刷新重试')
+  } else {
+    // 在设置请求时发生了一些事情，触发了一个错误
+    Message.error(`请求失败：${error.message}`)
+  }
+
+  // 把请求失败的错误对象继续抛出，扔给下一个上一个调用者
   return Promise.reject(error)
 })
 
